b6d793bd9d0d10b97d81915c42380b9882b96a40
amir-khan911 <noyaan911@gmail.com>
2024-08-02 03:12:20 -0700

    react js basic login and signup form

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './App.css'
+import LoginForm from './LoginForm'
+
+const App = () => {
+  return (
+   <>
+   
+    <LoginForm/>
+    
+   </>
+  )
+}
+
+export default App
